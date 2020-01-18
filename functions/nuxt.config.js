46c4a79a6e984a60d455f01f8deebdb779364d98
Hiromitsu Sasaki <sasaki.hiromitsu.eg65@members.co.jp>
2020-01-18 23:02:49 +0900

    Setup bulma as sass

--- functions/nuxt.config.js
+++ functions/nuxt.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  modules: [
+    'nuxt-buefy'
+  ],
+  css: [
+    { src: '~assets/main.scss', lang: 'scss' }
+  ],
   srcDir: 'nuxt-app',
   buildDir: 'nuxt-dist',
   build: {
